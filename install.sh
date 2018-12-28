8f3f3e2de484ebd04264387fdb724a56113faf78
mntzer12345 <46202766+mntzer12345@users.noreply.github.com>
2018-12-29 00:32:50 +0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,80 @@
+
+#!/bin/bash
+#MNTZER-ANWER
+#telegram MNTZER_18
+#instagram 313.JS
+
+red='\e[1;31m'
+green='\e[1;32m'
+blue='\e[1;34m'
+purple='\e[1;35m'
+cyan='\e[1;36m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+clear
+echo -e $green "-------------------------------------------------------"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                 $red INSTALL MNTZER    $green                 -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-                  $blue MNTZER-ANWER  $green                    -"
+sleep 0.3
+echo -e $green "-                                                     -"
+sleep 0.3
+echo -e $green "-------------------------------------------------------"
+sleep 0.5
+echo ""
+echo -e $red     "What useing?"
+echo
+echo -e $green   "1- Linux & Gnuroot"
+echo
+echo -e $green   "2- Termux"
+echo
+echo -e $green   "0- Exit"
+read use
+if [ $use = 2 ]
+then
+apt update && apt upgrade
+pkg install figlet -y
+pkg install git -y
+pkg install wget -y
+chmod +x MNTZER.sh
+pkg install toilet -y
+pkg install lolcat -y
+pkg install ruby -y
+gem install lolcat
+gem install figlet
+pip install wordlist
+pkg install nano -y
+pkg install termux-api -y
+pip install youtube-dl
+fi
+if [ $use = 1 ]
+then
+apt-get install figlet -y
+apt-get install git -y
+apt-get install wget -y
+apt-get install toilet -y
+chmod +x MNTZER.sh
+apt-get install toilet -y
+apt-get install lolcat -y
+apt-get install ruby
+gem install lolcat
+gem install figlet
+pip install wordlist
+apt-get install nano -y
+apt-get install termux-api -y
+pip install youtube-dl
+fi
+if [ $use = 0 ]
+then
+clear
+exit
+else
+bash install.sh
+fi
